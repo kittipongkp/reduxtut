ca65f88eac5d4a3bdcd681f34ca916843f3efc81
kittipong <kittipong.mesut@gmail.com>
2018-12-05 14:41:25 +0700

    store reducer dispatch subscribe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,3 +10,37 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
+
+import {createStore} from 'redux';
+
+const reducer=(state,action)=>{
+    switch (action.type) {
+        case "ADD":
+                state+=action.playload;
+            break;
+        case "SUBTRACT":
+                state-=action.playload;
+            break;
+    
+        default:
+            break;
+    }
+    return state;
+}
+const store=createStore(reducer,15000);
+
+store.subscribe(()=>{
+    console.log('Update store:',store.getState());
+});
+
+store.dispatch({
+    type:'ADD',
+    playload:500
+});
+
+store.dispatch({
+    type:'SUBTRACT',
+    playload:10000
+});
+
